71f05391429c58a95156752a90e5a715c57902ed
verenichyi <verenichyi@gmail.com>
2022-05-13 12:42:21 +0300

    refactor: use path module

--- index.js
+++ index.js
@@ -1,75 +1,75 @@
 const https = require('https');
-const { commandLineArgs, notFoundText } = require('./constants');
-const { sortByMostPopular, readFile, writeFile } = require('./helpers');
+const path = require('path');
+const {commandLineArgs, notFoundText} = require('./constants');
+const {sortByMostPopular, readFile, writeFile} = require('./helpers');
 
 const args = process.argv;
+const jokesPath = path.join(__dirname, 'jokes.json');
 
 const outputDadJoke = (search) => {
-  const options = {
-    hostname: 'icanhazdadjoke.com',
-    path: `/search?term=${search}`,
-    method: 'GET',
-    headers: {
-      Accept: 'application/json',
-    },
-  };
+	const options = {
+		hostname: 'icanhazdadjoke.com',
+		path: `/search?term=${search}`,
+		method: 'GET',
+		headers: {
+			Accept: 'application/json',
+		},
+	};
 
-  const request = https.request(options, (res) => {
-    const data = [];
+	const request = https.request(options, (res) => {
+		const data = [];
 
-    res
-      .on('data', (chunk) => {
-        data.push(chunk);
-      })
-      .on('end', async () => {
-        const jokes = JSON.parse(Buffer.concat(data).toString()).results;
+		res
+			.on('data', (chunk) => {
+				data.push(chunk);
+			})
+			.on('end', async () => {
+				const jokes = JSON.parse(Buffer.concat(data).toString()).results;
 
-        if (jokes.length) {
-          try {
-            const randomJoke =
-              jokes[Math.floor(Math.random() * jokes.length)].joke;
-            const data = await readFile('./jokes.json');
-            const fileData = JSON.parse(data.toString());
-            fileData.jokes.push(randomJoke);
+				if (jokes.length) {
+					try {
+						const randomJoke =
+							jokes[Math.floor(Math.random() * jokes.length)].joke;
+						const data = await readFile(jokesPath);
+						const fileData = JSON.parse(data.toString());
+						fileData.jokes.push(randomJoke);
 
-            process.stdout.write(`\nRandom joke: ${randomJoke}\n`);
-            await writeFile('./jokes.json', JSON.stringify(fileData));
-          } catch (error) {
-            process.stdout.write(error);
-          }
-        } else {
-          process.stdout.write(notFoundText);
-        }
-      });
-  });
+						process.stdout.write(`\nRandom joke: ${randomJoke}\n`);
+						await writeFile(jokesPath, JSON.stringify(fileData));
+					} catch (error) {
+						process.stdout.write(error);
+					}
+				} else {
+					process.stdout.write(notFoundText);
+				}
+			});
+	});
 
-  request.on('error', (error) => {
-    process.stdout.write(error);
-  });
+	request.on('error', (error) => {
+		process.stdout.write(error);
+	});
 
-  request.end();
+	request.end();
 };
 
 const outputLeaderboard = async () => {
-  const data = await readFile('./jokes.json');
-  const fileData = JSON.parse(data.toString());
+	const data = await readFile(jokesPath);
+	const fileData = JSON.parse(data.toString());
 
-  process.stdout.write(
-    `\nLeader board: ${sortByMostPopular(fileData.jokes)[0]}\n`
-  );
+	process.stdout.write(
+		`\nLeader board: ${sortByMostPopular(fileData.jokes)[0]}\n`
+	);
 };
 
 args.forEach((arg, index) => {
-  switch (arg) {
-    case commandLineArgs.searchTerm:
-      {
-        outputDadJoke(args[index + 1]);
-      }
-      break;
-    case commandLineArgs.leaderboard:
-      {
-        outputLeaderboard();
-      }
-      break;
-  }
+	switch (arg) {
+		case commandLineArgs.searchTerm: {
+			outputDadJoke(args[index + 1]);
+		}
+			break;
+		case commandLineArgs.leaderboard: {
+			outputLeaderboard();
+		}
+			break;
+	}
 });
