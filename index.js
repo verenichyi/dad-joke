04a5d00fe9c58d88e7d8162008103ea148cfbec8
verenichyi <verenichyi@gmail.com>
2022-05-23 13:13:37 +0300

    refactor: move some func from index file

--- index.js
+++ index.js
@@ -1,68 +1,9 @@
-const https = require('https');
-const path = require('path');
-const fs = require('fs').promises;
-const {
-  commandLineArgs,
-  notFoundText,
-  notFoundCommand,
-} = require('./constants');
-const { sortByMostPopular, logData } = require('./helpers');
+const { commandLineArgs, notFoundCommand } = require('./constants');
+const { logData } = require('./helpers');
+const outputLeaderboard = require('./outputLeaderboard');
+const outputDadJoke = require('./outputDadJoke');
 
 const args = process.argv.slice(2);
-const jokesPath = path.join(__dirname, 'jokes.json');
-
-const outputDadJoke = (search) => {
-  const options = {
-    hostname: 'icanhazdadjoke.com',
-    path: `/search?term=${search}`,
-    method: 'GET',
-    headers: {
-      Accept: 'application/json',
-    },
-  };
-
-  const request = https.request(options, (res) => {
-    const data = [];
-
-    res
-      .on('data', (chunk) => {
-        data.push(chunk);
-      })
-      .on('end', async () => {
-        const jokes = JSON.parse(Buffer.concat(data).toString()).results;
-
-        if (jokes.length) {
-          try {
-            const randomJoke =
-              jokes[Math.floor(Math.random() * jokes.length)].joke;
-            const data = await fs.readFile(jokesPath);
-            const fileData = JSON.parse(data.toString());
-            fileData.jokes.push(randomJoke);
-
-            logData(`\nRandom joke: ${randomJoke}\n`);
-            await fs.writeFile(jokesPath, JSON.stringify(fileData));
-          } catch (error) {
-            logData(error);
-          }
-        } else {
-          logData(notFoundText);
-        }
-      });
-  });
-
-  request.on('error', (error) => {
-    logData(error);
-  });
-
-  request.end();
-};
-
-const outputLeaderboard = async () => {
-  const data = await fs.readFile(jokesPath);
-  const fileData = JSON.parse(data.toString());
-
-  logData(`\nLeader board: ${sortByMostPopular(fileData.jokes)[0]}\n`);
-};
 
 args.forEach((arg, index) => {
   const { searchTerm, leaderboard } = commandLineArgs;
@@ -86,5 +27,3 @@ args.forEach((arg, index) => {
     }
   }
 });
-
-// (() => {})();
